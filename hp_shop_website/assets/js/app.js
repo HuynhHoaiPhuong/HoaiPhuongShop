67837048204bf571232b18f57abc35798d05e0ea
HuynhHoaiPhuong <huynhaiphuong001@gmail.com>
2022-05-18 22:56:45 +0700

    update UI 18/5/2022

---
+++ hp_shop_website/assets/js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.slideshow').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        infinity: true,
+    });
+    /* Menu fixed */
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= $(".header").height())
+        {
+            $(".menu").addClass("menu-fix");
+            $(".menu").css({position:"fixed",left:'0px',right:'0px',top:'0px',zIndex:'9999'});
+        }
+        else
+        {
+            $(".menu").removeClass("menu-fix");
+            $(".menu").css({position:"relative"});
+        }
+    });
+});
